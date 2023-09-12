fb74152ea928ed1bb880a7234407ac4e3b9ea87c
Hein Htet Kyaw <mr.kiddomonster@gmail.com>
2023-09-12 16:32:17 +0630

    rebuilding site Tue Sep 12 16:32:16 MST 2023

---
+++ image.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+find ./ -type f -name '*.png' -exec sh -c 'avifenc --min 10 --max 30 $1 "${1%.png}.avif"' _ {} \;
+
+find ./ -type f -name '*.jpg' -exec sh -c 'cwebp $1 -o "${1%.jpg}.webp"' _ {} \;
